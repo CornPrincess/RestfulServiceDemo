cf1809fd014e1b8fa2e35ff82e2609be5b9f29c6
ztb1211 <xiaocorn96@gmail.com>
2018-10-11 18:13:02 +0800

    add demo signin function

---
+++ src/main/java/com/example/restfulservice/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.example.restfulservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * @author corn
+ * @version 1.0.0
+ */
+
+@Controller
+public class LoginController {
+
+    @PostMapping(value = "/user/signin")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password,
+                        Map<String, Object> map) {
+        if(!StringUtils.isEmpty(username) && "123456".equals(password)) {
+            // 登录成功
+            return "dashboard";
+        } else {
+            // 登录失败
+            map.put("msg", "用户名密码错误");
+            return "signin";
+        }
+    }
+}
